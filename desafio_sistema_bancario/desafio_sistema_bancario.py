7e15eabe41470428fbbea349510c4032ca71f980
Ana Paula <anapaula.angelina@yahoo.com.br>
2024-05-16 16:42:06 -0300

    ajustes de pastas

--- desafio_sistema_bancario/desafio_sistema_bancario.py
+++ desafio_sistema_bancario/desafio_sistema_bancario.py
@@ -18,7 +18,7 @@ while True:
     opcao = input(menu)
 
     if opcao == "1":
-        valor = float(input("Informe o valor do depósito: "))
+        valor = float(input("Informe o valor a ser depositado: "))
 
         if valor > 0:
             saldo += valor
@@ -55,7 +55,7 @@ while True:
 
     elif opcao == "3":
         print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print("Não existe movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
 
@@ -63,4 +63,4 @@ while True:
         break
 
     else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("Operação falhou, por favor selecione novamente a operação desejada.")
